67feacd4b7c910efab5306366f7bd4aeaeb67703
SumanrajBera <srbera17@gmail.com>
2026-04-06 21:30:41 +0530

    Completed the backend of chat application

--- backend/server.js
+++ backend/server.js
@@ -13,22 +13,40 @@ wss.on("connection", (ws) => {
     ws.on("message", (data) => {
         data = JSON.parse(data)
         if (data.type === "chat") {
+            if (ws.username === "") {
+                ws.send(JSON.stringify({
+                    type: "chat",
+                    status: "error",
+                    message: "Please set a username"
+                }))
+                return
+            }
+
             if (ws.room === "") {
-                ws.send("Join a room or something")
+                ws.send(JSON.stringify({
+                    type: "chat",
+                    status: "error",
+                    message: "Join a room or something"
+                }))
                 return
             }
             wss.rooms.get(ws.room).forEach(client => {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    client.send(JSON.stringify({
+                if (client.readyState === WebSocket.OPEN) {
+                    let obj = {
+                        type: "chat",
+                        status: "success",
                         username: ws.username,
                         message: data.message
-                    }))
+                    }
+                    if (client === ws) obj.username = "Me"
+                    client.send(JSON.stringify(obj))
                 }
             });
         } else if (data.type === "join") {
             if (!wss.rooms.has(data.room)) {
                 ws.send(JSON.stringify({
                     type: "joinRoom",
+                    status: "error",
                     message: "Such room doesn't exist"
                 }))
                 return;
@@ -36,15 +54,26 @@ wss.on("connection", (ws) => {
             ws.room = data.room;
             let setOfClients = wss.rooms.get(data.room)
             setOfClients.add(ws)
-            console.log(setOfClients)
             ws.send(JSON.stringify({
                 type: "joinRoom",
+                status: "success",
                 message: `Welcome! You have joined ${ws.room}`
             }))
+
+            wss.rooms.get(ws.room).forEach(client => {
+                if (client !== ws && client.readyState === WebSocket.OPEN) {
+                    client.send(JSON.stringify({
+                        type: "joinRoom",
+                        status: "success",
+                        message: `${ws.username} has joined`
+                    }))
+                }
+            })
         } else if (data.type === "create") {
             if (wss.rooms.has(data.room)) {
                 ws.send(JSON.stringify({
                     type: "createRoom",
+                    status: "error",
                     message: "Room already exist!"
                 }))
                 return
@@ -53,14 +82,36 @@ wss.on("connection", (ws) => {
             wss.rooms.set(data.room, new Set([ws]))
             ws.send(JSON.stringify({
                 type: "createRoom",
+                status: "success",
                 message: `Welcome! You have created and joined ${ws.room}`
             }))
         } else if (data.type === "setUsername") {
             ws.username = data.username;
+            ws.send(JSON.stringify({
+                type: "setUsername",
+                status: "success",
+                message: `Username is set to ${ws.username}`
+            }))
         }
     })
 
     ws.on("error", (err) => {
         console.log(err)
+    })
+
+    ws.on("close", () => {
+        if (!ws.room) return
+        const setOfClients = wss.rooms.get(ws.room)
+        if (!setOfClients) return
+        setOfClients.forEach(client => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({
+                    type: "terminate",
+                    status: "success",
+                    message: `${ws.username || "Someone"} has exited`
+                }))
+            }
+        })
+        setOfClients.delete(ws)
     })
 })
